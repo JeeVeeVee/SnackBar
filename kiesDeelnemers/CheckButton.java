8e8c11f244841f99319d540b82214e102368474d
juvervae <jules.vervaeke@ugent.be>
2020-07-24 12:56:21 +0200

    the program runs, i can acces the row data from the button, checkboxes still lag. Havent been able to change graphic to image

--- kiesDeelnemers/CheckButton.java
+++ kiesDeelnemers/CheckButton.java
@@ -1,13 +1,23 @@
 package kiesDeelnemers;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class CheckButton extends Button implements Listener {
 
     private KiesDeelnemersModel model;
+    private ImageView checkView;
 
     public CheckButton(KiesDeelnemersModel model, Deelnemer deelnemer){
         this.model = model;
+        checkView = new ImageView();
+        super.getChildren().add(checkView);
+        super.setGraphic(checkView);
+        super.setOnAction(e -> {
+            CheckCell parent = (CheckCell) super.getParent();
+            System.out.println(parent.getDeelnemer().getLeider().toString());
+        });
     }
 
     public CheckButton(){
